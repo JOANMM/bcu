b6a57c36843b7296eecc73958a155db6d7346e8b
dxw <dxw525224259@163.com>
2015-05-23 17:12:44 +0800

    kezhitheme

---
+++ lang/en/theme_bcu.php
@@ -0,0 +1,252 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * @package    theme
+ * @subpackage bcu
+ * @copyright  2014 Birmingham City University <michael.grant@bcu.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ */
+
+$string['choosereadme'] = '
+<div class="clearfix">
+<div class="well">
+<h2>BCU Responsive</h2>
+<p><img class=img-polaroid src="bcu/pix/screenshot.png" /></p>
+</div>
+<div class="well">
+<h3>About</h3>
+<p>This is a modified Moodle bootstrap theme which inherits styles and renderers from its parent theme.</p>
+<h3>Parents</h3>
+<p>This theme is based upon the Bootstrap theme, which was created for Moodle 2.6</p>
+</div></div>';
+
+$string['configtitle'] = 'BCU';
+$string['region-middle'] = 'middle';
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = '你在这个文本区域中加上的任何css规则都会反映在每个页面，简化为定制的主题';  
+
+
+
+$string['pluginname'] = 'BCU';
+
+$string['region-side-post'] = '右边';
+$string['region-side-pre'] = '左边';
+$string['region-footer-block'] = '底部';
+$string['region-main-top'] = '内容顶部';
+$string['region-banere-top'] = 'banner';
+
+$string['fsize'] = '全局字体大小';
+$string['fsizedesc'] = '适应全局字体大小（用百分比，仅使用数字，默认大小是85）';
+
+$string['p1'] = 'Picture1';
+$string['p1desc'] = '在网页幻灯片中添加图片，输入图片的URL.';
+
+$string['p2'] = 'Picture2';
+$string['p2desc'] = '在网页幻灯片中添加图片，输入图片的URL.';
+
+$string['p3'] = 'Picture3';
+$string['p3desc'] = '在网页幻灯片中添加图片，输入图片的URL.';
+
+$string['p4'] = 'Picture4';
+$string['p4desc'] = '在网页幻灯片中添加图片，输入图片的URL.';
+
+$string['p5'] = 'Picture5';
+$string['p5desc'] = '在网页幻灯片中添加图片，输入图片的URL.';
+
+$string['enablehelp'] = '帮助链接';
+$string['enablehelpdesc'] = '在导航栏中加入帮助链接';
+
+$string['p1cap'] = 'Picture1 说明';
+$string['p1capdesc'] = '给网页图片添加说明，为说明标题使用H3标题格式';
+
+$string['p2cap'] = 'Picture2 说明';
+$string['p2capdesc'] = '给网页图片添加说明，为说明标题使用H3标题格式';
+
+$string['p3cap'] = 'Picture3 说明';
+$string['p3capdesc'] = '给网页图片添加说明，为说明标题使用H3标题格式';
+
+$string['p4cap'] = 'Picture4 说明';
+$string['p4capdesc'] = '给网页图片添加说明，为说明标题使用H3标题格式';
+
+$string['p5cap'] = 'Picture5 说明';
+$string['p5capdesc'] = '给网页图片添加说明，为说明标题使用H3标题格式';
+
+$string['p1url'] = '滑块 1 URL';
+$string['p1urldesc'] = '为滑块1添加一个URL';
+
+$string['p2url'] = '滑块 2 URL';
+$string['p2urldesc'] = '为滑块2添加一个URL';
+
+$string['p3url'] = '滑块 3 URL';
+$string['p3urldesc'] = '为滑块3添加一个URL';
+
+$string['p4url'] = '滑块 4 URL';
+$string['p4urldesc'] = '为滑块4添加一个URL';
+
+$string['p5url'] = '滑块 5 URL';
+$string['p5urldesc'] = '为滑块5添加一个URL';
+
+$string['logo'] = 'Logo';
+$string['logodesc'] = '通过输入一个URL来改变这个主题的logo，（也就是http://www.somesite/animage.png）一个透明的png格式的会更好';
+
+$string['linkcolor'] = '链接颜色';
+$string['linkcolordesc'] = '为这个主题里的链接设置颜色，使用html颜色代码';
+
+$string['linkhover'] = '鼠标悬停链接颜色';
+$string['linkhoverdesc'] = '为这个主题里的链接设置鼠标悬停时的颜色，使用html颜色代码';
+
+$string['backcolor'] = '背景颜色';
+$string['backcolordesc'] = '设置背景颜色';
+
+$string['maincolor'] = '主要颜色';
+$string['maincolordesc'] = '块和页脚的主要颜色';
+
+$string['footnote'] = '脚注';
+$string['footnotedesc'] = '给页脚添加文本';
+
+$string['enablemy'] = '我的仪表板';
+$string['enablemydesc'] = '显示MyMoodle页面链接';
+
+$string['enableprofile'] = '用户配置文件';
+$string['enableprofiledesc'] = '显示用户配置文件的链接';
+
+$string['enableeditprofile'] = '编辑用户配置文件';
+$string['enableeditprofiledesc'] = '显示编辑用户配置文件的链接';
+
+$string['enablebadges'] = '识别证';
+$string['enablebadgesdesc'] = '显示用户识别的链接';
+
+$string['enablecalendar'] = '用户日历';
+$string['enablecalendardesc'] = '显示用户日历的链接';
+
+$string['enableprivatefiles'] = '私人文件';
+$string['enableprivatefilesdesc'] = '显示用户私人文件的链接';
+
+$string['market1'] = '营销块 1';
+$string['market1desc'] = '为营销块1添加html（参见readme文件附加信息和提示）';
+
+$string['market2'] = '营销块 2';
+$string['market2desc'] = '为营销块2添加html（参见readme文件附加信息和提示）';
+
+$string['market3'] = '营销块 3';
+$string['market3desc'] = '为营销块3添加html（参见readme文件附加信息和提示）';
+
+$string['market4'] = '营销块 4';
+$string['market4desc'] = '为营销块4添加html（参见readme文件附加信息和提示）';
+
+
+$string['infobox'] = '网页信息框';
+$string['infoboxdesc'] = '网页信息块HTML（参见readme文件附加信息和提示）';
+
+$string['infobox2'] = '网页次要信息框';
+$string['infobox2desc'] = '网页次要信息框（参见readme文件附加信息和提示）';
+
+$string['noenrolments'] = '没有找到登记.';
+
+$string['alertbox'] = '顶部的警告框';
+$string['alertboxdesc'] = '警告框显示在所有页面的顶部';
+
+$string['jssection'] = 'Javascript 部分';
+$string['jssectiondesc'] = '添加javascript代码到网站,例如谷歌分析';
+
+$string['footer1header'] = '页脚第一节标题';
+$string['footer2header'] = '页脚第二节标题';
+$string['footer3header'] = '页脚第三节标题';
+$string['footer4header'] = '页脚第四节标题';
+
+$string['footer1desc'] = '在页脚区域一添加一个标题';
+$string['footer2desc'] = '在页脚区域二添加一个标题';
+$string['footer3desc'] = '在页脚区域三添加一个标题';
+$string['footer4desc'] = '在页脚区域四添加一个标题';
+
+$string['footer1content'] = '页脚第一节的内容';
+$string['footer2content'] = '页脚第二节的内容';
+$string['footer3content'] = '页脚第三节的内容';
+$string['footer4content'] = '页脚第四节的内容';
+
+$string['footer1contentdesc'] = '在页脚区域一添加内容';
+$string['footer2contentdesc'] = '在页脚区域二添加内容';
+$string['footer3contentdesc'] = '在页脚区域三添加内容';
+$string['footer4contentdesc'] = '在页脚区域四添加内容';
+
+$string['customcssjssettings'] = '自定义 CSS & JS';
+$string['genericsettingsheading'] = '应用自己的修改';
+$string['genericsettingsdescription'] = '在这里你可以找到各种设置把自己的CSS和JavaScript代码添加到主题.';
+
+$string['slideshowsettings'] = '幻灯片';
+$string['slideshowsettingsheading'] = '自定义首页上的carousel';
+$string['slideshowdesc'] = '上传图片，添加链接和描述在首页上的carcousel.';
+
+$string['marketingsettings'] = '营销';
+$string['marketingsettingsheading'] = '自定义营销块出现在首页上';
+$string['marketingdesc'] = '您可以构建营销块在首页直到4块,列如,请参见主题附带的README文件.';
+
+
+$string['footersettings'] = '页脚';
+$string['footersettingsheading'] = '设置的内容应该出现在页脚.';
+$string['footerdesc'] = '控制的内容应该出现在页面的第4个页脚部分';
+
+$string['navbarsettings'] = '导航栏';
+$string['navbarsettingsheading'] = '自定义导航栏';
+$string['navbardesc'] = '允许您控制的所有元素出现在导航栏上.';
+
+$string['coloursettings'] = '颜色';
+$string['coloursettingsheading'] = '修改整个主题中使用的主要颜色.';
+$string['colourdesc'] = '你可以选择在整个主题中你想使用的颜色.';
+
+$string['headersettings'] = '标题';
+$string['headersettingsheading'] = '自定义这一主题的标题.';
+$string['headerdesc'] = '显示警告框提醒用户的动作,或为所有用户提供通知。控制出现在网站的导航栏.';
+
+$string['thiscourse'] = '本课程';
+
+$string['frontpageblocksettings'] = '网页模块';
+$string['frontpageslidersettings'] = '网页制作滑块';
+$string['frontpagecoursesettings'] = '网页制作课程';
+$string['frontpagesettingsheading'] = '网页呈现';
+$string['frontpagedesc'] = '配置课程框呈现在网页的方式';
+$string['frontpagerenderer'] = '网页制作课程框';
+$string['frontpagerendererdesc'] = '控制课程框呈现在首页的方式';
+$string['frontpagerendereroption1'] = 'Tiles';
+$string['frontpagerendereroption2'] = 'Tiles w/ 覆盖';
+
+$string['rendereroverlaycolour'] = '覆盖颜色';
+$string['rendereroverlaycolourdesc'] = '当"Tiles w/ 覆盖"渲染器被选中的时候覆盖的颜色 ';
+
+$string['rendereroverlayfontcolour'] = '覆盖的字体颜色';
+$string['rendereroverlayfontcolourdesc'] = '当鼠标悬停在有"Tiles w/ 覆盖"渲染器的课程框时的字体的颜色';
+
+$string['buttoncolour'] = '按钮的颜色';
+$string['buttoncolourdesc'] = '在整个网站中使用的主要按钮的颜色.';
+
+$string['buttonhovercolour'] = '按钮的颜色 (当鼠标悬停在按钮的上方时)';
+$string['buttonhovercolourdesc'] = '当鼠标悬停在按钮的上方时按钮的颜色变化.';
+
+$string['frontpagerendererdefaultimage'] = '默认课程图片';
+$string['frontpagerendererdefaultimagedesc'] = '当没有找到课程图片时使用默认图片, (只适用于Tiles w/ 覆盖)';
+$string['loadtime'] = '页面加载';
+
+$string['sitetitle'] = '网站标题';
+$string['sitetitledesc'] = '显示/隐藏网站标题';
+
+$string['hideblocks'] = '隐藏模块';
+$string['showblocks'] = '显示模块';
+$string['fullscreen'] = '全屏';
+$string['standardview'] = '标准观点';
